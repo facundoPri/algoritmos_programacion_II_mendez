1c233c3c498c4e62402afbd850f4d426b9fd2a45
Facundo Prieto <facundo.prieto321@gmail.com>
2021-07-19 15:01:11 -0300

    nombre funciones pruebas

--- tps/tp2_salon_de_la_fama_reloaded/pruebas.c
+++ tps/tp2_salon_de_la_fama_reloaded/pruebas.c
@@ -1,10 +1,51 @@
 #include "pa2mm.h"
+#include "src/lista.h"
 #include "src/salon.h"
 
-int main(){
+// Salon leer archivos
+void DadoArchivoInexistente_CuandoIntentoLeerArchivo_EntoncesObtengoNULL() {}
 
-    pa2m_nuevo_grupo("Alguna prueba");
+void DadoUnArchivoExistente_CuandoIntentoLeerArchivo_EntoncesNoObtengoNULL() {}
 
+// Salon guardar
+void DadoUnSalonNULL_CuandoIntentoGuardarSalon_EntoncesObtengo0() {}
 
-    return pa2m_mostrar_reporte();
+void DadoUnSalonConEntrenadores_CuandoGuardoSalon_EntoncesObtengoCantidadDeEntrenadores() {
+  // crea salon
+  // guarda salon
+  // crea salon con archivo guardado
+  // guarda nuevamente
+  // si la cantidad corresponde entonces guardar y salvar funcionan
+}
+
+// Salon_agregar_entrenador
+void DadoUnSalonOEntrenadorNull_CuandoIntentoAgregarEntrenador_EntoncesObtengoNULL() {
+
+}
+
+void DadoUnSalonYEntrenador_CuandoAgregoEntrenador_EntoncesLaCantidadDeEntrenadoresIncrementa() {
+
+}
+
+// Filtrar salon
+void DadoUnSalonOFuncionNULL_CuandoIntentoFiltrarSalon_EntoncesObtengoNULL() {}
+
+void DadoUnSalonYFuncion_CuandoFiltroSalon_EntoncesObtengoListaConElementosDeseados() {
+
+}
+
+// Salon Ejecutar Comandos
+void DadoUnSalonOComandoNULL_CuandoIntentoEjecutarComando_EntoncesObtengoNULL() {
+
+}
+
+void DadoUnSalonYComandoNoExistente_CuandoIntentoEjecutarComando_EntoncesObtengoNULL() {
+
+}
+
+int main() {
+
+  pa2m_nuevo_grupo("Alguna prueba");
+
+  return pa2m_mostrar_reporte();
 }
